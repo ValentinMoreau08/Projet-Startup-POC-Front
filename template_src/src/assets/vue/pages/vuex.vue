b5b9dd1589c388b9f0577f2658649faa7db422f9
bubunzer <sobunung@yahoo.fr>
2021-02-03 12:36:58 +0100

    #2 added login feature

--- template_src/src/assets/vue/pages/vuex.vue
+++ template_src/src/assets/vue/pages/vuex.vue
@@ -1,33 +1,94 @@
 <template>
-  <f7-page>
-    <f7-navbar title="About F7" back-link="Back" sliding></f7-navbar>
-    <f7-block-title>{{user.name ? "You are already logged" : "Please login"}}</f7-block-title>
-    <f7-list>
-      <f7-list-group v-if="user.name">
-        <f7-list-item>ID: {{user.id}}</f7-list-item>
-        <f7-list-item>Username: {{user.name}}</f7-list-item>
-      </f7-list-group>
-    </f7-list>
-    <f7-list>
-      <f7-list-button color="blue" v-on:click="loginAction">Login Button</f7-list-button>
-    </f7-list>
-  </f7-page>
+  <div class="page no-navbar no-toolbar no-swipeback">
+    <div class="page-content login-screen-content">
+      <div class="login-screen-title">Startup POC Project</div>
+      <form>
+        <div class="list">
+          <ul>
+            <li class="item-content item-input">
+              <div class="item-inner">
+                <div class="item-title item-label">Username</div>
+                <div class="item-input-wrap">
+                  <input type="text" name="username" placeholder="Your username" v-model="username"/>
+                </div>
+              </div>
+            </li>
+            <li class="item-content item-input">
+              <div class="item-inner">
+                <div class="item-title item-label">Password</div>
+                <div class="item-input-wrap">
+                  <input type="password" name="password" placeholder="Your password" v-model="password"/>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="list">
+          <ul>
+            <li><a href="#" v-on:click="loginAction" class="list-button" >Sign In</a></li>
+          </ul>
+          <div class="block-footer">
+            <p><a href="#" class="link back">Close Login Screen</a></p>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 <script>
 import { mapState, mapActions } from "vuex";
+import Axios from "axios";
 
 export default {
+  data() {
+      return {
+          users:[],
+          username: '',
+          password: ''
+      }
+    },
+  created() {
+    Axios.get("http://localhost:8180"+'/users').then(response => {
+        this.users = response.data
+        //console.log(response.data)
+      }).catch(
+      function (error) {
+        console.log("oups")
+      }
+    );
+  },
   components: {},
   computed: {
     ...mapState({
       user: state => state.user
     })
   },
+
   methods: {
+    
     loginAction: function() {
+      const { username, password } = this;
       const self = this;
-      // Set new user into storage
-      self.userLogged({ name: "User", id: "#1" });
+          const app = self.$f7;
+          const router = self.$f7router;
+
+      this.$store.dispatch("login", {
+        username: this.username,
+        password: this.password
+      })
+      this.users.forEach(function(user){
+        if (username == user.login && password == user.password) {
+          
+          router.back(`/`, {
+              ignoreCache: true,
+              force: true,
+              context: {}
+            });
+        }
+        else{
+          
+        }
+      })
     },
     ...mapActions(["userLogged"])
   }
